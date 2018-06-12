9ab4b8c9f511d647299a760196042e45d4788557
Samad <abdulsamadf97@gmail.com>
2018-06-13 01:34:54 +0500

    feat: Make a grid

--- designs.js
+++ designs.js
@@ -5,6 +5,25 @@
 
 function makeGrid() {
 
-// Your code goes here!
+    let tableHeight = $("#inputHeight").val();
+    let tableWidth = $("#inputWidth").val();
+    $("#inputHeight").attr("value", tableHeight);
+    $("#inputWidth").attr("value", tableWidth);
+
+    for (let i = 0; i < tableHeight; i++) {
+        //create many elements and set their location once each
+        let tableRow = $("<tr></tr>")
+        $("#pixelCanvas").append(tableRow)
+        for (let j = 0; j < tableWidth; j++) {
+            //create many elements and set their location once each
+            let tableColumnData = $("<td></td>")
+            tableRow.append(tableColumnData)
+        }
+    }
 
 }
+
+$("#sizePicker").on("submit", function (event) {
+    event.preventDefault()
+    makeGrid();
+})
